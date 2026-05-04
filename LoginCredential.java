747cd361e0bb20ec2156a571e4ad92b0bca46eee
1mplying <abacsbasc@gmail.com>
2026-05-04 13:59:10 -0700

    Add LoginCredential class for user authentication

---
+++ LoginCredential.java
@@ -0,0 +1,29 @@
+public class LoginCredential extends Credential{
+
+    private String username;
+    private String password;
+    private String url;
+
+    public LoginCredential(String label, String username, String password, String url){
+        super(label);
+        this.username = username;
+        this.password = password;
+        this.url = url;
+    }
+        public String getUsername(){
+            return username;
+        }
+
+        public String getPassword(){
+            return password;
+        }
+
+        public String getUrl(){
+            return url;
+        }
+
+        @Override
+        public String getsummary() {
+            return "Login Credential: " + getLabel() + " for " + url;
+        }
+    }
